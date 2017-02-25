64426f2d0d0c7ccccd7d23e2cee4f2f1b3e250c3
Niyazi Motor <niyazimotor@gmail.com>
2017-02-25 14:28:04 +0300

    COM-25 JobServiceTest düzenlendi.

--- src/test/java/service/JobServiceTest.java
+++ src/test/java/service/JobServiceTest.java
@@ -1,36 +1,49 @@
 package service;
 
-import config.PersistenceContextTest;
+import config.JobServiceTestConfig;
 import config.ServiceContext;
+import dto.JobListDto;
 import jpa.JobRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
 /**
  * Created by Nyomoto on 11.2.2017.
  */
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {PersistenceContextTest.class, ServiceContext.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JobServiceTestConfig.class})
 public class JobServiceTest {
-/*
-    @Autowired
-    JobRepository jobRepository;
 
     @Autowired
-    JobService jobService;
+    JobRepository repository;
+
+    @Autowired
+    JobService service;
 
     @Test
-    public void jobService_IsNotNull() {
-        assertNotNull(jobService);
+    public void service_IsNotNull() {
+        assertNotNull(service);
     }
-*/
+
+    @Test
+    public void findAll_ShouldReturnemptyList() {
+        given(repository.findAll()).willReturn(new ArrayList<>());
+        //when(repository.findAll()).thenReturn(new ArrayList<>());
+
+        List<JobListDto> list = service.getAll();
+
+        assertEquals(0, list.size());
+    }
+
 }
