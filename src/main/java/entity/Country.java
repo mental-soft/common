c96f9d12e35bc4e1be142ab618802c0f11fe764d
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-03-16 00:26:21 +0300

    COM-43 kalite araçlarının hepsi hazırlandı. checksyle builde eklendi.

--- src/main/java/entity/Country.java
+++ src/main/java/entity/Country.java
@@ -17,168 +17,169 @@ import java.util.List;
 @Entity
 @Table(name = "COUNTRY")
 @SequenceGenerator(name = "ID_SEQ",
-        sequenceName = "SEQ_COUNTRY_ID",
-        allocationSize = 1)
+    sequenceName = "SEQ_COUNTRY_ID",
+    allocationSize = 1)
 public class Country {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ID_SEQ")
-    @Column(name = "ID")
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ID_SEQ")
+  @Column(name = "ID")
+  private Integer id;
 
-    @Column(name = "NAME", columnDefinition = "NVARCHAR2")
-    private String name;
+  @Column(name = "NAME", columnDefinition = "NVARCHAR2")
+  private String name;
 
-    @Column(name = "CODE", columnDefinition = "NVARCHAR2")
-    private String code;
+  @Column(name = "CODE", columnDefinition = "NVARCHAR2")
+  private String code;
 
-    @Column(name = "EN_NAME", columnDefinition = "NVARCHAR2")
-    private String enName;
+  @Column(name = "EN_NAME", columnDefinition = "NVARCHAR2")
+  private String enName;
 
-    @Column(name = "IS_ACTIVE", columnDefinition = "NUMBER")
-    private Boolean isActive;
+  @Column(name = "IS_ACTIVE", columnDefinition = "NUMBER")
+  private Boolean isActive;
 
-    @Column(name = "CREATED_DATE", columnDefinition = "TIMESTAMP")
-    private Date createdDate;
+  @Column(name = "CREATED_DATE", columnDefinition = "TIMESTAMP")
+  private Date createdDate;
 
-    @Column(name = "MODIFIED_DATE", columnDefinition = "TIMESTAMP")
-    private Date modifiedDate;
+  @Column(name = "MODIFIED_DATE", columnDefinition = "TIMESTAMP")
+  private Date modifiedDate;
 
-    @OneToMany(mappedBy = "country",cascade = CascadeType.ALL)
-    private List<City> cities;
+  @OneToMany(mappedBy = "country", cascade = CascadeType.ALL)
+  private List<City> cities;
 
-    @PreUpdate
-    public void preUpdate(){
-        modifiedDate = new Date();
+  @PreUpdate
+  public void preUpdate() {
+    modifiedDate = new Date();
+  }
+
+  @PrePersist
+  public void prePersist() {
+    createdDate = new Date();
+  }
+
+  public Country() {
+    cities = new ArrayList<>();
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getEnName() {
+    return enName;
+  }
+
+  public void setEnName(String enName) {
+    this.enName = enName;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Date getModifiedDate() {
+    return modifiedDate;
+  }
+
+  public void setModifiedDate(Date modifiedDate) {
+    this.modifiedDate = modifiedDate;
+  }
+
+  public Boolean getActive() {
+    return isActive;
+  }
+
+  public void setActive(Boolean active) {
+    isActive = active;
+  }
+
+  public List<City> getCities() {
+    return cities;
+  }
+
+  public void setCities(List<City> cities) {
+    this.cities = cities;
+  }
+
+
+  public static CountryBuilder getBuilder() {
+    return new CountryBuilder();
+
+  }
+
+  public static class CountryBuilder {
+
+
+    private Country country;
+
+    public CountryBuilder() {
+      country = new Country();
     }
 
-    @PrePersist
-    public void prePersist(){
-        createdDate = new Date();
+    public CountryBuilder id(Integer id) {
+      this.country.setId(id);
+      return this;
     }
 
-    public Country() {
-        cities = new ArrayList<>();
+    public CountryBuilder name(String name) {
+      this.country.setName(name);
+      return this;
     }
 
-    public Integer getId() {
-        return id;
+    public CountryBuilder enName(String enName) {
+      this.country.setEnName(enName);
+      return this;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public CountryBuilder code(String code) {
+      this.country.setCode(code);
+      return this;
     }
 
-    public String getName() {
-        return name;
+    public CountryBuilder modifiedDate(Date modifiedDate) {
+      this.country.setModifiedDate(modifiedDate);
+      return this;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public CountryBuilder createdDate(Date createdDate) {
+      this.country.setCreatedDate(createdDate);
+      return this;
     }
 
-    public String getCode() {
-        return code;
+    public CountryBuilder active(Boolean active) {
+      this.country.setActive(active);
+      return this;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getEnName() {
-        return enName;
-    }
-
-    public void setEnName(String enName) {
-        this.enName = enName;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(Date modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-    public List<City> getCities() {
-        return cities;
-    }
-
-    public void setCities(List<City> cities) {
-        this.cities = cities;
-    }
-
-
-    public static CountryBuilder getBuilder() {
-        return new CountryBuilder();
+    public Country build() {
+      return country;
 
     }
 
-    public static class CountryBuilder {
-
-
-        private Country country;
-
-        public CountryBuilder() {
-            country = new Country();
-        }
-
-        public CountryBuilder id(Integer id) {
-            this.country.setId(id);
-            return this;
-        }
-
-        public CountryBuilder name(String name) {
-            this.country.setName(name);
-            return this;
-        }
-
-        public CountryBuilder enName(String enName) {
-            this.country.setEnName(enName);
-            return this;
-        }
-
-        public CountryBuilder code(String code) {
-            this.country.setCode(code);
-            return this;
-        }
-
-        public CountryBuilder modifiedDate(Date modifiedDate) {
-            this.country.setModifiedDate(modifiedDate);
-            return this;
-        }
-
-        public CountryBuilder createdDate(Date createdDate) {
-            this.country.setCreatedDate(createdDate);
-            return this;
-        }
-
-        public CountryBuilder active(Boolean active) {
-            this.country.setActive(active);
-            return this;
-        }
-        public Country build() {
-            return country;
-
-        }
-
-    }
+  }
 
 }
