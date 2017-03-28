b914c65accd0b0cb91568612b479c0c980608a2c
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-03-28 23:15:29 +0300

    COM-33 country controller testte çıkan hatalar giderildi. get country testi yapıldı.

--- src/test/java/config/CountryControllerTestConfig.java
+++ src/test/java/config/CountryControllerTestConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import service.CityService;
 import service.CountryService;
 import service.CountryServiceImpl;
--- src/test/java/controller/CountryControllerTest.java
+++ src/test/java/controller/CountryControllerTest.java
@@ -1,11 +1,17 @@
 package controller;
 
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Map;
 
 import config.CountryControllerTestConfig;
 import config.SpringWebContext;
@@ -22,6 +28,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import service.CountryService;
@@ -30,35 +37,45 @@ import service.CountryService;
 /**
  * Created by Coskun on 25.3.2017.
  */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {CountryControllerTestConfig.class, SpringWebContext.class})
-//@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {CountryControllerTestConfig.class, SpringWebContext.class})
+@WebAppConfiguration
 public class CountryControllerTest {
 
-  //  @Autowired
-  //  private CountryService countryService;
+  @Autowired
+  private CountryService countryService;
 
-  //private MockMvc mockMvc;
+  private MockMvc mockMvc;
 
-  //  @Autowired
-  //  private WebApplicationContext webApplicationContext;
+  @Autowired
+  private WebApplicationContext webApplicationContext;
 
-  //  @Before
-  //  public void setUp() {
-  //    Mockito.reset(countryService);
-  //    //mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-  //  }
+  @Before
+  public void setUp() {
+    Mockito.reset(countryService);
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+  }
 
-  //  @Test
-  //  public void showAddCountryForm_ShouldCreateFormObjectAndRenderAddCountryForm()
-  // throws Exception {
-  //    mockMvc.perform(get(CountryController.REQUEST_MAPPING_COUNTRY))
-  //        .andExpect(status().isOk())
-  //        .andExpect(view().name(CountryController.VIEW_COUNTRY_ADD))
-  //        .andExpect(forwardedUrl("/WEB-INF/views/country/add.html"));
-  //
-  //    verifyZeroInteractions(countryService);
-  //  }
+  @Test
+  public void showAddCountryForm_ShouldCreateFormObjectAndRenderAddCountryForm()
+      throws Exception {
+    ResultActions result = mockMvc.perform(get(CountryController.REQUEST_MAPPING_COUNTRY))
+        //.andDo(print())  Gelen sonucu konsola basar.
+        .andExpect(status().isOk())
+        .andExpect(view().name(CountryController.VIEW_COUNTRY_ADD))
+        .andExpect(model().size(1))
+        .andExpect(model().hasNoErrors());
+
+    Map<String, Object> model = result.andReturn().getModelAndView().getModel();
+    CountryDto countryDto = (CountryDto) model.get("countryDto");
+    assertNull(countryDto.getId());
+    assertNull(countryDto.getName());
+    assertNull(countryDto.getCode());
+    assertNull(countryDto.getEnName());
+    assertNull(countryDto.getIsActive());
+
+    verifyZeroInteractions(countryService);
+  }
 
 
 }
