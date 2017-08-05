aea95e0b7e20630a578f3f0b982c94d878aa5fe0
Erhan KARAKAYA <erhan277@gmail.com>
2017-08-05 16:31:42 +0300

    COM-49 Fixed base entity

--- src/main/java/com/teammental/common/dal/entity/BaseEntity.java
+++ src/main/java/com/teammental/common/dal/entity/BaseEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public abstract class BaseEntity<IdT> {
+public abstract class BaseEntity<IdT extends Number> {
 
   @Column(name = "id")
   @Id
