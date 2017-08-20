d80f734323632e4df9ec8e588ce7179c82730b94
Erhan KARAKAYA <erhan277@gmail.com>
2017-08-20 22:15:20 +0300

    COM-58 removed genericValue table

--- src/main/java/com/teammental/common/dal/repository/GenericValueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teammental.common.dal.repository;
-
-import com.teammental.common.dal.entity.GenericValue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenericValueRepository extends JpaRepository<GenericValue, Integer> {
-}
